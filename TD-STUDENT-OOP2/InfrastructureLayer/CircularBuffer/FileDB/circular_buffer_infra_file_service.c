3d433a5d9f8e742d14931a343430852aaea0eb21
Antoine GRAND <Antoine>
2022-10-17 20:00:50 +0200

    add files

--- TD-STUDENT-OOP2/InfrastructureLayer/CircularBuffer/FileDB/circular_buffer_infra_file_service.c
+++ TD-STUDENT-OOP2/InfrastructureLayer/CircularBuffer/FileDB/circular_buffer_infra_file_service.c
@@ -40,20 +40,76 @@ static FILE *data_stream;
 
 int ICircularBufferRepository_save(circular_buffer cb)
 {
+    if (!ICircularBufferRepository_open(FILE_DB_REPO))
+        return 0;
+    ICircularBufferRepository_append(cb);
+    return 1;
 }
 
 void ICircularBufferRepository_close(void)
 {
+    fclose(data_stream);
+    fclose(index_stream);
 }
 
 static FILE *auxiliary_open(char *prefix, char *suffix)
 {
+    int prefix_length = strlen(prefix);
+    int suffix_length = strlen(suffix);
+    char name[prefix_length + suffix_length + 1];
+    strncpy(name, prefix, prefix_length);
+    strncpy(name + prefix_length, suffix, suffix_length +1);
+    FILE *stream = fopen(name, "r+");
+    if (stream == NULL)
+        stream = fopen(name, "w+");
+    if (stream == NULL)
+        perror(name);
+    return stream;
 }
 
 int ICircularBufferRepository_open(char *name)
 {
+    data_stream = auxiliary_open(name, DATA_SUFFIX);
+    if (data_stream == NULL)
+        return 0;
+    index_stream = auxiliary_open(name, INDEX_SUFFIX);
+    if(index_stream == NULL)
+    {
+        fclose(index_stream);
+        return 0;
+    }
+    return 1;
 }
 
+static void auxiliary_get_offset(circular_buffer cb, struct RECORD_CB *Record)
+{
+    char * current = cb -> current;
+    char * head = cb -> head;
+    cb -> current = cb -> tail;
+    
+    for (int i = 0 ; i<cb -> length ; i++)
+    {
+        if (cb -> current == current)
+            Record -> offset_current = i;
+        if (cb -> head == head)
+            Record -> offset_head = i;
+        Record->data[i] = *(cb -> current);
+        (cb -> current)++;
+    }
+
+
+}
 int ICircularBufferRepository_append(circular_buffer cb)
 {
+    struct index index;
+    struct RECORD_CB myRecord;
+    auxiliary_get_offset(cb, &myRecord);
+    size_t length = sizeof(myRecord);
+    fseek(data_stream, 0L, SEEK_END);
+    index.recordStart = ftell(data_stream);
+    index.recordLength = length;
+    fwrite(myRecord.data, sizeof(int), 3, data_stream);
+    fseek(index_stream, 0L, SEEK_END);
+    fwrite(&index, sizeof index, 1, index_stream);
+    return 1;
 }
